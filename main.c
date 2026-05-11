916ac6422c001891302c934cb2592bfe5cb7255e
Anthony Dias Soares <44712777+ZonyDS@users.noreply.github.com>
2026-05-11 01:05:16 +0000

    adição de If Else

--- main.c
+++ main.c
@@ -10,4 +10,13 @@ int main() {
     printf("Bolinha\n");
     printf("Bolinha 2\n");
     return 0;
+
+    int numero = 5;
+
+    if (numero > 0) {
+        printf("O número é positivo.\n");
+    } else {
+        printf("O número é negativo ou zero.\n");
+    }
+    return 0;
 }
